faaa18cfd14f85463bcde897173cc0208bff875d
czuellig <cedric.zuellig@gmail.com>
2024-04-28 21:35:50 +0200

    kein code nur vibes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 
-let bildurl = "https://picsum.photos/600/600";
+let imageUrl = "https://picsum.photos/600/600";
+document.querySelector("#image").src = imageUrl;
 
 
 
@@ -9,13 +10,5 @@ let bildurl = "https://picsum.photos/600/600";
 document.querySelector("#refresh").addEventListener("click", function() {
     location.reload();
 });
-// Eine Funktion, die an die Variable bildurl den Text "?grayscale" anhängt, wenn die Checkbox "grayscale" aktiviert ist.
-function updateImageUrl() {
-    if (document.querySelector("#grayscale").checked) {
-        bildurl += "?grayscale";
-    }
-}
 
-// Event Listener für die Checkbox "grayscale"
-document.querySelector("#grayscale").addEventListener("change", updateImageUrl);
 
